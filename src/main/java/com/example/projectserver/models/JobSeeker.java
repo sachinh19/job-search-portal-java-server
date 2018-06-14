8638045dbb10cb31b84b5e53b18440ea90f4ce49
Aparna Sharma <aparnasharma285@gmail.com>
2018-06-13 20:19:57 -0400

    job seeker model

---
+++ src/main/java/com/example/projectserver/models/JobSeeker.java
@@ -0,0 +1,31 @@
+package com.example.projectserver.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+@Entity
+public class JobSeeker extends Person{
+
+    @Getter
+    @Setter
+    private String interestedPosition;
+
+    @Getter
+    @Setter
+    private String totalExp;
+
+
+    @ManyToMany(mappedBy = "jobSeekers")
+    @Getter
+    @Setter
+    private List<JobType> jobTypes;
+
+
+
+
+}
