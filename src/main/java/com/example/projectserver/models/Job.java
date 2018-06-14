38be5415d47a74ec566208bcabfd5f4730f25c23
Aparna Sharma <aparnasharma285@gmail.com>
2018-06-13 22:57:03 -0400

    job model

---
+++ src/main/java/com/example/projectserver/models/Job.java
@@ -0,0 +1,75 @@
+package com.example.projectserver.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "job_id")
+    @Getter
+    @Setter
+    private int id;
+
+    @Getter
+    @Setter
+    private String position;
+
+    @Getter
+    @Setter
+    private String city;
+
+    @Getter
+    @Setter
+    private String country;
+
+    @Column(name = "description", length = 10000)
+    @Getter
+    @Setter
+    private String description;
+
+
+    @Getter
+    @Setter
+    private String postedDate;
+
+    @Getter
+    @Setter
+    private int totalApplications = 0;
+
+    @Getter
+    @Setter
+    private boolean expired = false;
+
+    @ManyToOne
+    @JsonIgnore
+    @Getter
+    @Setter
+    private Company company;
+
+    @ManyToMany(mappedBy = "jobs")
+    @Getter
+    @Setter
+    private List<JobType> jobTypes;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "person_job_table", joinColumns = @JoinColumn(name = "person_id"), inverseJoinColumns = @JoinColumn(name = "job_id"))
+    @JsonIgnore
+    @Getter
+    @Setter
+    private List<Person> persons;
+
+
+    @OneToMany(mappedBy = "job",cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    @Getter
+    @Setter
+    private List<Query> queries;
+
+}
