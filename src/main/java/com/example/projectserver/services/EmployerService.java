2f82939cd19fab3c2d109ae0add82a658d1956bf
Aparna Sharma <aparnasharma285@gmail.com>
2018-06-14 03:19:30 -0400

    Employer and JobSeeker service

---
+++ src/main/java/com/example/projectserver/services/EmployerService.java
@@ -0,0 +1,122 @@
+package com.example.projectserver.services;
+
+import com.example.projectserver.models.Company;
+import com.example.projectserver.models.Employer;
+import com.example.projectserver.models.Role;
+import com.example.projectserver.repositories.CompanyRepository;
+import com.example.projectserver.repositories.EmployerRepository;
+import com.example.projectserver.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class EmployerService {
+
+    private EmployerRepository employerRepository;
+    private RoleRepository roleRepository;
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    public EmployerService(EmployerRepository employerRepository,RoleRepository roleRepository, CompanyRepository companyRepository){
+        this.employerRepository = employerRepository;
+        this.roleRepository = roleRepository;
+        this.companyRepository = companyRepository;
+    }
+
+
+    @GetMapping("api/employer")
+    public List<Employer> findAllEmployers() {
+        return employerRepository.findAll();
+    }
+
+    @GetMapping("api/employer/{employerId}")
+    public Employer findEmployerById(@PathVariable("employerId") int employerId) {
+
+        return employerRepository.findById(employerId).orElse(null);
+    }
+
+    @PostMapping("api/register/employer")
+    public Employer createEmployer(@RequestBody Employer employer) {
+
+        employer.setRoleType("Employer");
+        Role role = roleRepository.findRoleByName("Employer").orElse(null);
+
+        String companyName = employer.getCompanyName();
+        Company company = companyRepository.findCompanyByName(companyName).orElse(null);
+
+        if(role != null){
+
+            employer.setRole(role);
+        }
+
+        if(company != null){
+            employer.setCompany(company);
+        }
+
+        return employerRepository.save(employer);
+    }
+
+    @PutMapping("api/employer/{employerId}")
+    public Employer updateEmployer(@PathVariable("employerId") int employerId,
+                             @RequestBody Employer newEmployer,
+                             HttpServletResponse response) {
+
+        Employer existingEmployer = employerRepository.findById(employerId).orElse(null);
+
+        if (existingEmployer != null) {
+            String username = newEmployer.getUsername();
+            String password = newEmployer.getPassword();
+            String firstName = newEmployer.getFirstName();
+            String lastName = newEmployer.getLastName();
+            String email = newEmployer.getEmail();
+            String position = newEmployer.getPosition();
+            String tenure = newEmployer.getTenure();
+            Company company = newEmployer.getCompany();
+            String expDescription = newEmployer.getExpDescription();
+            String aboutMe = newEmployer.getAboutMe();
+
+            if (username != null) {
+                existingEmployer.setUsername(username);
+            }
+
+            if (password != null) {
+                existingEmployer.setPassword(password);
+            }
+            if (firstName != null) {
+                existingEmployer.setFirstName(firstName);
+            }
+            if (lastName != null) {
+                existingEmployer.setLastName(lastName);
+            }
+            if (email != null) {
+                existingEmployer.setEmail(email);
+            }
+
+            if(position != null){
+                existingEmployer.setPosition(position);
+            }
+            if(tenure != null){
+                existingEmployer.setTenure(tenure);
+            }
+            if(company != null){
+
+                existingEmployer.setCompany(company);
+            }
+            if(expDescription != null){
+                existingEmployer.setExpDescription(expDescription);
+            }
+            if(aboutMe != null){
+                existingEmployer.setAboutMe(aboutMe);
+            }
+
+            return employerRepository.save(existingEmployer);
+        }
+
+        response.setStatus(204);
+        return existingEmployer;
+    }
+}
---
+++ src/main/java/com/example/projectserver/services/JobSeekerService.java
@@ -0,0 +1,105 @@
+package com.example.projectserver.services;
+
+import com.example.projectserver.models.JobSeeker;
+import com.example.projectserver.models.Role;
+import com.example.projectserver.repositories.JobSeekerRepository;
+import com.example.projectserver.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class JobSeekerService {
+
+    private JobSeekerRepository jobSeekerRepository;
+    private RoleRepository roleRepository;
+
+    @Autowired
+    JobSeekerService(JobSeekerRepository jobSeekerRepository,RoleRepository roleRepository) {
+        this.jobSeekerRepository = jobSeekerRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @GetMapping("api/jobseeker")
+    public List<JobSeeker> findAllJobSeekers() {
+        return jobSeekerRepository.findAll();
+    }
+
+    @GetMapping("api/jobseeker/{jobseekerId}")
+    public JobSeeker findJobSeekerById(@PathVariable("jobseekerId") int jobseekerId) {
+
+        return jobSeekerRepository.findById(jobseekerId).orElse(null);
+    }
+
+    @PostMapping("api/register/jobseeker")
+    public JobSeeker createJobSeeker(@RequestBody JobSeeker jobSeeker) {
+
+        jobSeeker.setRoleType("JobSeeker");
+        Role role = roleRepository.findRoleByName("JobSeeker").orElse(null);
+        if(role != null){
+
+            jobSeeker.setRole(role);
+        }
+        return jobSeekerRepository.save(jobSeeker);
+    }
+
+    @PutMapping("api/jobseeker/{jobseekerId}")
+    public JobSeeker updateJobSeeker(@PathVariable("jobseekerId") int jobseekerId,
+                             @RequestBody JobSeeker newJobSeeker,
+                             HttpServletResponse response) {
+
+        JobSeeker existingJobSeeker = jobSeekerRepository.findById(jobseekerId).orElse(null);
+
+        if (existingJobSeeker != null) {
+            String username = newJobSeeker.getUsername();
+            String password = newJobSeeker.getPassword();
+            String firstName = newJobSeeker.getFirstName();
+            String lastName = newJobSeeker.getLastName();
+            String email = newJobSeeker.getEmail();
+            String interestedPosition = newJobSeeker.getInterestedPosition();
+            String totalExp = newJobSeeker.getTotalExp();
+            String expDescription = newJobSeeker.getExpDescription();
+            String aboutMe = newJobSeeker.getAboutMe();
+
+
+            if (username != null) {
+                existingJobSeeker.setUsername(username);
+            }
+
+            if (password != null) {
+                existingJobSeeker.setPassword(password);
+            }
+            if (firstName != null) {
+                existingJobSeeker.setFirstName(firstName);
+            }
+            if (lastName != null) {
+                existingJobSeeker.setLastName(lastName);
+            }
+            if (email != null) {
+                existingJobSeeker.setEmail(email);
+            }
+
+            if(interestedPosition != null){
+                existingJobSeeker.setInterestedPosition(interestedPosition);
+            }
+            if(totalExp != null){
+                existingJobSeeker.setTotalExp(totalExp);
+            }
+            if(expDescription != null){
+                existingJobSeeker.setExpDescription(expDescription);
+            }
+            if(aboutMe != null){
+                existingJobSeeker.setAboutMe(aboutMe);
+            }
+
+            return jobSeekerRepository.save(existingJobSeeker);
+        }
+
+        response.setStatus(204);
+        return existingJobSeeker;
+    }
+
+}
