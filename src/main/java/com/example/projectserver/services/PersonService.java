2b8a27e83e449f70bdd493cbb0f7b00264b0c772
Aparna Sharma <aparnasharma285@gmail.com>
2018-06-25 06:43:55 -0400

    person service changed to get current user

--- src/main/java/com/example/projectserver/services/PersonService.java
+++ src/main/java/com/example/projectserver/services/PersonService.java
@@ -17,37 +17,52 @@ public class PersonService {
     private PersonRepository personRepository;
 
     @Autowired
-    public PersonService(PersonRepository personRepository){
+    public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
     @GetMapping("api/person/{personId}")
-    public Person findPersonById(@PathVariable("personId")int personId,HttpServletResponse response) {
+    public Person findPersonById(@PathVariable("personId") int personId, HttpServletResponse response) {
         Person person = personRepository.findById(personId).orElse(null);
-        if(person!=null)
+        if (person != null) {
             return person;
-        response.setStatus(204);
-        return null;
+        } else {
+            response.setStatus(204);
+            return null;
+        }
+
+    }
+
+    @GetMapping("api/person/currentuser")
+    public Person findCurrentUser(HttpSession session, HttpServletResponse response) {
+        Person currentUser = (Person) session.getAttribute("currentUser");
+
+        if (currentUser != null) {
+            return currentUser;
+        } else {
+            response.setStatus(204);
+            return null;
+        }
     }
 
     @GetMapping("api/person/username/{username}")
-    public Person findPersonByUsername(@PathVariable("username")String username,HttpServletResponse response) {
-                Person existingPerson = personRepository.findPersonByUsername(username).orElse(null);
-        if(existingPerson!=null)
+    public Person findPersonByUsername(@PathVariable("username") String username, HttpServletResponse response) {
+        Person existingPerson = personRepository.findPersonByUsername(username).orElse(null);
+        if (existingPerson != null)
             return existingPerson;
         response.setStatus(204);
         return null;
     }
 
     @GetMapping("api/person")
-    public List<Person> findAllPersons(HttpServletResponse response)    {
-                return personRepository.findAll();
+    public List<Person> findAllPersons(HttpServletResponse response) {
+        return personRepository.findAll();
     }
 
     @PostMapping("api/login")
-    public Person login(@RequestBody Person person, HttpServletRequest request, HttpServletResponse response){
-                Person existingPerson = personRepository.findPersonByCredentials(person.getUsername(), person.getPassword()).orElse(null);
-        if(existingPerson != null){
+    public Person login(@RequestBody Person person, HttpServletRequest request, HttpServletResponse response) {
+        Person existingPerson = personRepository.findPersonByCredentials(person.getUsername(), person.getPassword()).orElse(null);
+        if (existingPerson != null) {
             HttpSession session = request.getSession(true);
             response.setStatus(200);
             session.setAttribute("currentUser", existingPerson);
@@ -61,14 +76,14 @@ public class PersonService {
     @GetMapping("api/logout")
     public void logout(HttpSession session, HttpServletResponse response) {
         session.invalidate();
-            }
+    }
 
     @DeleteMapping("api/person/{personId}")
-    public void deletePerson(@PathVariable("personId") int personId, HttpServletResponse response){
+    public void deletePerson(@PathVariable("personId") int personId, HttpServletResponse response) {
 
         Person existingPerson = personRepository.findById(personId).orElse(null);
-        if(existingPerson != null){
+        if (existingPerson != null) {
             personRepository.delete(existingPerson);
         }
-            }
+    }
 }
